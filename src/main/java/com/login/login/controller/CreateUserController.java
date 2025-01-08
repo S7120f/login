6e13896bddc9411f3f319a2a1fe9a1e66231a76f
Your Namejonas <skonby_jonas@hotmail.com>
2025-01-08 21:29:56 +0100

    created controll class för creating new user and created my SecurityConfig for filter different request in my application, filtred that you can visit homepage and register siter without being logged in

--- src/main/java/com/login/login/controller/CreateUserController.java
+++ src/main/java/com/login/login/controller/CreateUserController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class CreateUserController {
 
-    @GetMapping("/home")
-    public String home(){
-        return "/home";
+    @GetMapping("/register")
+    public String showRegistrationPage() {
+
+        return "register";
     }
+
+ 
+    
 }
