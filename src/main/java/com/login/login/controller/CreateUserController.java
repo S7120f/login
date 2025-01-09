7ed1801ccfb283d99013f0890eb16f5e8985ee92
Your Namejonas <skonby_jonas@hotmail.com>
2025-01-09 22:55:07 +0100

    new inventory page visible in logged in mode, and logout button visible when logge d in and login button visible when logged out

--- src/main/java/com/login/login/controller/CreateUserController.java
+++ src/main/java/com/login/login/controller/CreateUserController.java
@@ -2,14 +2,16 @@ package com.login.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+// @RequestMapping("/register")
 public class CreateUserController {
 
     @GetMapping("/register")
     public String showRegistrationPage() {
 
-        return "register";
+        return "/register";
     }
 
  
