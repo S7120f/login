f2fb9204d7e9911a54476d4db5eebfb24f0b1267
Your Namejonas <skonby_jonas@hotmail.com>
2025-01-11 15:11:44 +0100

    we are now able to create user and save it in our database, the password will be encrypted

--- src/main/java/com/login/login/repository/UserRepository.java
+++ src/main/java/com/login/login/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.login.login.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.dto.UserDto;
 import com.login.login.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
+
+    public User findByUsername (String username); // hitta användare via användarnamn
+
+    User save(UserDto userDto);
     
 }
--- src/main/java/com/login/login/service/SecurityService.java
+++
@@ -1,5 +0,0 @@
-package com.login.login.service;
-
-public interface SecurityService { // skapar SecurityService för att tillhandahålla aktuell inloggad användare
-    boolean isAuthenticated();
-}
